c35110d57ee4e0889681e537e97881119571ba8b
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:28:56 +0530

    Delete modelsfolder/modelproject directory

--- modelsfolder/modelproject/testapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from testapp.models import employee
-from testapp.models import student
-
-# Register your models here.
-admin.site.register(employee)
-admin.site.register(student)

b5ab477bbb6b966c207a7144ebb65b311acba072
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:20:18 +0530

    Delete dynamicrender directory

--- dynamicrender/dynamicpro/dynamicapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-# Create your views here.
-def display(request):
-    dt=datetime.datetime.now()
-    ke={'date':dt}
-    return render(request,'dynamicapp/results.html',context=ke)

b2baaabf783f045523ca195dc71e99d0a2104530
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:23:19 +0530

    Delete f/day9/forms1 directory

--- f/day9/forms1/formapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def display(request):
-    return render(request,'formapp/login.html')
-def display1(request):
-    return render(request,'formapp/register.html')
-def display2(request):
-    return render(request,'formapp/submit.html')

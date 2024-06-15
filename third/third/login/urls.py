d3aa892f6e22ddcd2e5ba3b5db1666f656ad2c18
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:26:06 +0530

    Delete third/third directory

--- third/third/login/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from login import views as v1
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('a/', v1.datetimes),
-]
--- third/third/register/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from register import views as v2
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('b/', v2.display),
-]

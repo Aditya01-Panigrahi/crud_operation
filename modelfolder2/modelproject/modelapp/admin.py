0e9a1d4ce06e6f42c17401b52d3d970a5d093a65
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:25:24 +0530

    Delete modelfolder2/modelproject directory

--- modelfolder2/modelproject/modelapp/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from modelapp.models import student
-# Register your models here.
-admin.site.register(student)
--- modelfolder2/modelproject/modelapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class student(models.Model):
-    sid=models.IntegerField()
-    sname=models.CharField(max_length=20)
-    srollno=models.IntegerField()
-    sfees=models.IntegerField()
-    dob=models.DateField()

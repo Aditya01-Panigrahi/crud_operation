b724ea77b535ee6289fae4a7e03d70092656a870
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:22:35 +0530

    Delete retrive/retirvepro directory

--- retrive/retirvepro/testapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from testapp.models import Employee
-
-def displaydate(request):
-    employees=Employee.objects.all()
-    return render (request,'testapp/result.html',{'employees':employees})
-# Create your views here.

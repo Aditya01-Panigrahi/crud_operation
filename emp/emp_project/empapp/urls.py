fbbb9de3f0b2bf8c21be9ac3352579baa2e7c3a3
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:25:37 +0530

    Delete emp/emp_project directory

--- emp/emp_project/empapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('empname/startswith/<str:search_string>/', views.search_empname_startswith, name='search_empname_startswith'),
-    path('empname/contains/<str:search_string>/', views.search_empname_contains, name='search_empname_contains'),
-    path('empage/lte/<int:age>/', views.search_empage_lte, name='search_empage_lte'),
-]

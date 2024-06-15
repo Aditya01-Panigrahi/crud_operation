b446c7e39d225975a59c7fa3e2edff8b41d22a04
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:22:19 +0530

    Delete fourth/fourth directory

--- fourth/fourth/register/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render,HttpResponse
-import datetime
-
-# Create your views here.
-def datetimes(request):
-    a='<h1> the time is:'+str(datetime.datetime.now())
-    return HttpResponse(a)

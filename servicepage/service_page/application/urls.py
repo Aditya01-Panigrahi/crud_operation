01d6a6902cbc327009bdc80977fae05ee18e9b50
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:28:33 +0530

    Delete servicepage/service_page directory

--- servicepage/service_page/application/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('services/', views.service_page, name='service_page'),
-    # Add more URL patterns as needed
-]

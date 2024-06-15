9dce413433860a05e3653cd3c4387a332cad7a75
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:22:52 +0530

    Delete image/imageproject directory

--- image/imageproject/imageapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-# Create your views here.
-def imagesdisplay(request):
-    dt=datetime.datetime.now()
-    k={'date':dt}
-    return render(request,"imageapp/result.html",context=k)

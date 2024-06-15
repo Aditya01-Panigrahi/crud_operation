52e61f3024ad690c69dc1b1a8962081b9e639e40
Aditya01-Panigrahi <159413477+Aditya01-Panigrahi@users.noreply.github.com>
2024-06-15 18:21:58 +0530

    Delete form2/form_project directory

--- form2/form_project/form_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request,'form_app/home.html')
-def login(request):
-    return render(request,'form_app/login.html')
